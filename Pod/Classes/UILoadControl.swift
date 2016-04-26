cbae8b7364722f32e2de5241786ba9180c61d189
Tuan Le <tuanla@mobilus.co.jp>
2016-04-26 17:45:03 +0900

    Turning codes

--- Pod/Classes/UILoadControl.swift
+++ Pod/Classes/UILoadControl.swift
@@ -10,144 +10,143 @@ import UIKit
 import Foundation
 
 public class UILoadControl: UIControl {
-  
-  private let activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.Gray)
-  
-  public var heightLimit: CGFloat = 80.0
-  public private (set) var loading: Bool = false
-  
-  var scrollView: UIScrollView = UIScrollView()
-  
-  override public var frame: CGRect {
-    didSet{
-      if (frame.size.height > heightLimit) && !loading {
-        self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
-      }
+    
+    private var activityIndicatorView: UIActivityIndicatorView?
+    
+    public var heightLimit: CGFloat = 80.0
+    
+    public private (set) var loading: Bool = false {
+        didSet {
+            /*
+             Set layout to a "loading" or "not loading" state
+             */
+            dispatch_async(dispatch_get_main_queue()) { [unowned self] in
+                
+                var contentInset = self.scrollView.contentInset
+                
+                if self.loading {
+                    contentInset.bottom = self.heightLimit
+                    self.activityIndicatorView?.startAnimating()
+                }else{
+                    contentInset.bottom = 0.0
+                    self.activityIndicatorView?.stopAnimating()
+                }
+                
+                self.scrollView.contentInset = contentInset
+            }
+        }
     }
-  }
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    self.initialize()
-  }
-  
-  required public init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    self.initialize()
-  }
-  
-  override public func awakeFromNib() {
-    super.awakeFromNib()
-  }
-  
-  override public func layoutSubviews() {
-    super.layoutSubviews()
-  }
-
-  /*
-  Update layout at finsih to load
-  */
-  public func endLoading(){
-    setLoading(false)
-    fixPosition()
-  }
-  
+    
+    var scrollView: UIScrollView = UIScrollView()
+    
+    override public var frame: CGRect {
+        didSet{
+            if (frame.size.height > heightLimit) && !loading {
+                self.sendActionsForControlEvents(UIControlEvents.ValueChanged)
+            }
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initialize(activityIndicatorStyle: .Gray)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initialize(activityIndicatorStyle: .Gray)
+    }
+    
+    convenience init(target: AnyObject, action: Selector, style: UIActivityIndicatorViewStyle = .Gray) {
+        self.init()
+        self.initialize(activityIndicatorStyle: style)
+    }
+    
+    /*
+     Initilize the control
+     */
+    private func initialize(activityIndicatorStyle style: UIActivityIndicatorViewStyle) {
+        /*
+         Prepare activityIndicator
+         */
+        let activityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: style)
+        activityIndicatorView.hidesWhenStopped = false
+        activityIndicatorView.transform = CGAffineTransformMakeScale(1.4, 1.4)
+        
+        self.addSubview(activityIndicatorView)
+        self.bringSubviewToFront(activityIndicatorView)
+        self.activityIndicatorView = activityIndicatorView
+        
+        self.reloadTargetsIfNedeed()
+    }
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    /*
+     Update layout at finsih to load
+     */
+    public func endLoading() {
+        self.loading = false
+        self.fixPosition()
+    }
+    
 }
 
 extension UILoadControl {
-  
-  /*
-  Initilize the control
-  */
-  private func initialize(){
-    setupActivityIndicator()
-    self.reloadTargetsIfNedeed()
-  }
-  
-  
-  /*
-  Make sure that the trigger target is active
-  */
-  private func reloadTargetsIfNedeed() {
-    let selectorName: Selector = "didValueChange:"
-    if self.actionsForTarget(self, forControlEvent: UIControlEvents.ValueChanged) == nil {
-      self.addTarget(self, action: selectorName, forControlEvents: UIControlEvents.ValueChanged)
-    }
-  }
-  
-  
-  /*
-  Check if the control frame should be updated.
-  This method is called after user hits the end of the scrollView
-  */
-  func updateUI(){
-    if self.scrollView.contentSize.height < self.scrollView.bounds.size.height {
-      return
+    /*
+     Make sure that the trigger target is active
+     */
+    private func reloadTargetsIfNedeed() {
+        let selectorName: Selector = "didValueChange:"
+        if self.actionsForTarget(self, forControlEvent: UIControlEvents.ValueChanged) == nil {
+            self.addTarget(self, action: selectorName, forControlEvents: UIControlEvents.ValueChanged)
+        }
     }
     
-    let contentOffSetBottom = max(0, ((scrollView.contentOffset.y + scrollView.frame.size.height) - scrollView.contentSize.height))
-    if (contentOffSetBottom >= 0 && !loading) || (contentOffSetBottom >= heightLimit && loading) {
-      self.updateFrame(CGRectMake(0.0, scrollView.contentSize.height, scrollView.frame.size.width, contentOffSetBottom))
-    }
-  }
-  
-  /*
-  Update layout after user scroll the scrollView
-  */
-  private func updateFrame(rect: CGRect){
-    guard let superview = self.superview else {
-      return
+    
+    /*
+     Check if the control frame should be updated.
+     This method is called after user hits the end of the scrollView
+     */
+    func updateUI() {
+        if self.scrollView.contentSize.height < self.scrollView.bounds.size.height {
+            return
+        }
+        
+        let contentOffSetBottom = max(0, ((scrollView.contentOffset.y + scrollView.frame.size.height) - scrollView.contentSize.height))
+        if (contentOffSetBottom >= 0 && !loading) || (contentOffSetBottom >= heightLimit && loading) {
+            self.updateFrame(CGRectMake(0.0, scrollView.contentSize.height, scrollView.frame.size.width, contentOffSetBottom))
+        }
     }
     
-    superview.frame = rect
-    frame = superview.bounds
-    activityIndicatorView.alpha = (((frame.size.height * 100) / heightLimit) / 100)
-    activityIndicatorView.center = CGPointMake((frame.size.width / 2), (frame.size.height / 2))
-  }
-  
-  /*
-  Place control at the scrollView bottom
-  */
-  private func fixPosition(){
-    self.updateFrame(CGRectMake(0.0, scrollView.contentSize.height, scrollView.frame.size.width, 0.0))
-  }
-  
-  /*
-  Set layout to a "loading" or "not loading" state
-  */
-  private func setLoading(isLoading: Bool){
-    loading = isLoading
-    dispatch_async(dispatch_get_main_queue()) { [unowned self] in
-      
-      var contentInset = self.scrollView.contentInset
-      
-      if self.loading {
-        contentInset.bottom = self.heightLimit
-        self.activityIndicatorView.startAnimating()
-      }else{
-        contentInset.bottom = 0.0
-        self.activityIndicatorView.stopAnimating()
-      }
-      
-      self.scrollView.contentInset = contentInset
+    /*
+     Update layout after user scroll the scrollView
+     */
+    private func updateFrame(rect: CGRect) {
+        guard let superview = self.superview else {
+            return
+        }
+        
+        superview.frame = rect
+        frame = superview.bounds
+        self.activityIndicatorView?.alpha = (((frame.size.height * 100) / heightLimit) / 100)
+        self.activityIndicatorView?.center = CGPointMake((frame.size.width / 2), (frame.size.height / 2))
     }
-  }
-  
-  /*
-  Prepare activityIndicator
-  */
-  private func setupActivityIndicator(){
     
-    self.activityIndicatorView.hidesWhenStopped = false
-    self.activityIndicatorView.color = UIColor.darkGrayColor()
-    self.activityIndicatorView.transform = CGAffineTransformMakeScale(1.4, 1.4)
+    /*
+     Place control at the scrollView bottom
+     */
+    private func fixPosition() {
+        self.updateFrame(CGRectMake(0.0, scrollView.contentSize.height, scrollView.frame.size.width, 0.0))
+    }
     
-    addSubview(self.activityIndicatorView)
-    bringSubviewToFront(self.activityIndicatorView)
-  }
-  
-  @objc private func didValueChange(sender: AnyObject?){
-    setLoading(true)
-  }
-  
+    @objc private func didValueChange(sender: AnyObject?) {
+        self.loading = true
+    }
 }
